15b6a543a4e4baa743cf65e0a42d8eaa9130dc02
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-11-03 12:08:03 +0100

    Fix type checking and improved period loads

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -8,6 +8,7 @@ import (
 	"github.com/urfave/cli"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -49,43 +50,50 @@ var Fetch = cli.Command{
 }
 
 type cal struct {
+	debug  bool
 	types  []string
 	weekly bool
 	err    logFn
 	log    logFn
 }
 
-func New(types ...string) *cal {
+func New(debug bool, types ...string) (*cal, error) {
 	logFn := func(s string, args ...interface{}) {
 		fmt.Printf(s, args...)
 		fmt.Println()
 	}
 	errFn := func(s string, args ...interface{}) {
-		fmt.Fprintf(os.Stderr, s , args...)
+		fmt.Fprintf(os.Stderr, s, args...)
 		fmt.Fprintln(os.Stderr)
 	}
+	valTypes := ValidTypes()
+	rem := make([]int, 0)
+	for i, typ := range types {
+		valid := false
+		for _, valTyp := range valTypes {
+			if strings.ToLower(typ) == strings.ToLower(valTyp) {
+				valid = true
+				break
+			}
+		}
+		if !valid {
+			errFn("invalid type %s", typ)
+			rem = append(rem, i)
+		}
+	}
+	for i := len(rem)-1; i >= 0; i-- {
+		types = append(types[:i], types[i+1:]...)
+	}
+	if len(types) == 0 {
+		return nil, fmt.Errorf("no valid types found")
+	}
 	return &cal{
+		debug:  debug,
 		types:  types,
 		weekly: true,
 		log:    logFn,
 		err:    errFn,
-	}
-}
-
-func GetTypes(types ...string) []string {
-	fetchTypes := make([]string, 0)
-	for _, cal := range types {
-		if cal == liquid.LabelTeamLiquid {
-			//fetchTypes = append(fetchTypes, liquid.ValidTypes[:]...)
-			fetchTypes = append(fetchTypes, cal)
-		} else if cal == plusforward.LabelPlusForward {
-			//fetchTypes = append(fetchTypes, plusforward.ValidTypes[:]...)
-			fetchTypes = append(fetchTypes, cal)
-		} else {
-			fetchTypes = append(fetchTypes, cal)
-		}
-	}
-	return fetchTypes
+	}, nil
 }
 
 func ValidTypes() []string {
@@ -107,42 +115,33 @@ type toLoad struct {
 	t string
 }
 
-func (c cal) Load(startDate time.Time, period time.Duration) (calendar.Events, error) {
+func (c cal) Load(startDate time.Time) (calendar.Events, error) {
 	events := make(calendar.Events, 0)
 	urls := make([]toLoad, 0)
 	date := startDate
-	endDate := startDate.Add(period)
-	for {
-		if endDate.Sub(date) <= 0 {
-			break
+	for _, typ := range c.types {
+		if plusforward.ValidType(typ) {
+			url, err := plusforward.GetCalendarURL(date, typ, true)
+			if err != nil {
+				c.err("unable to get calendar URI for %s: %s", typ, err)
+				continue
+			}
+			urls = append(urls, toLoad{u: url, d: date, t: typ,})
 		}
-		for _, typ := range c.types {
-			validType := false
-			if plusforward.ValidType(typ) {
-				url, err := plusforward.GetCalendarURL(date, typ, true)
-				if err != nil {
-					//c.err("unable to get calendar URI for %s: %s", typ, err)
-					continue
-				}
-				validType = true
-				urls = append(urls, toLoad{u: url, d: date, t: typ,})
+		if liquid.ValidType(typ) {
+			url, err := liquid.GetCalendarURL(date, typ, true)
+			if err != nil {
+				c.err("unable to get calendar URI for %s: %s", typ, err)
+				continue
 			}
-			if liquid.ValidType(typ) {
-				url, err := liquid.GetCalendarURL(date, typ, true)
-				if err != nil {
-					//c.err("unable to get calendar URI for %s: %s", typ, err)
-					continue
-				}
-				validType = true
-				urls = append(urls, toLoad{u: url, d: date, t: typ,})
-			}
-			if !validType {
-				c.err("invalid type %s", typ)
-			}
+			urls = append(urls, toLoad{u: url, d: date, t: typ,})
 		}
-		date = date.Add(7 * 24 * time.Hour)
 	}
+
 	for _, l := range urls {
+		if c.debug {
+			c.log("[%s] %s", l.t, l.u)
+		}
 		if plusforward.ValidType(l.t) {
 			ev, err := plusforward.LoadEvents(l.u, l.d)
 			if err != nil {
@@ -150,6 +149,9 @@ func (c cal) Load(startDate time.Time, period time.Duration) (calendar.Events, e
 				continue
 			}
 			events = append(events, ev...)
+			if c.debug {
+				c.log("%d events", len(ev))
+			}
 		}
 		if liquid.ValidType(l.t) {
 			ev, err := liquid.LoadEvents(l.u, l.d)
@@ -158,14 +160,20 @@ func (c cal) Load(startDate time.Time, period time.Duration) (calendar.Events, e
 				continue
 			}
 			events = append(events, ev...)
+			if c.debug {
+				c.log("%d events", len(ev))
+			}
 		}
 	}
+
 	return events, nil
 }
 
+const durationStep = 7 * 24 * time.Hour
+
 func fetchCalendars(c *cli.Context) error {
-	types := GetTypes(c.StringSlice("calendar")...)
-	f := New(types...)
+	types := c.StringSlice("calendar")
+
 	start := time.Now().Add(-1 * defaultDuration)
 	if sf := c.String("start"); len(sf) > 0 {
 		if sfp, err := time.Parse("2006-01-02", sf); err == nil {
@@ -173,7 +181,48 @@ func fetchCalendars(c *cli.Context) error {
 		}
 	}
 	duration := c.Duration("end")
-	events, err := f.Load(start, duration)
-	fmt.Printf("%#v", events)
+	debug := c.Bool("debug")
+
+	f, err := New(debug, types...)
+	if err != nil {
+		return err
+	}
+
+	date := start
+	endDate := start.Add(duration - time.Second)
+
+	var events calendar.Events
+	for {
+		duration :=  durationStep - time.Second
+		if debug {
+			f.log("Loading events for period: %s - %s", date.Format("2006-01-02 Mon, 15:04"), date.Add(duration).Format("2006-01-02 Mon, 15:04"))
+		}
+		events, err = f.Load(date)
+		if debug {
+			for _, e := range events {
+				fmtTime := e.StartTime.Format("2006-01-02 15:04 MST")
+				cat := ""
+				stg := ""
+				fm := "%s%s%s"
+				if len(e.Category) > 0 {
+					cat = e.Category
+					fm = "%s:%s%s"
+
+				}
+				if len(e.Stage) > 0 {
+					stg = e.Stage
+					fm = "%s:%s:%s"
+				}
+				f.log("[%d] " + fm + " @ %s//%s", e.CalID, e.Type, cat, stg, fmtTime, e.Duration)
+				if e.Content != "" {
+					f.log("%v", e.Content)
+				}
+			}
+		}
+		if endDate.Sub(date) <= durationStep {
+			break
+		}
+		date = date.Add(duration)
+	}
 	return err
 }
