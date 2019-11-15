1e6eb99b1571c0a39e7ebc74729abb9a1968ae45
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-11-15 21:16:06 +0100

    Added some meat on the toot command, using mckael/madon

--- cmd/toot.go
+++ cmd/toot.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/McKael/madon"
 	"github.com/urfave/cli"
+	"os"
 )
 
 var Toot = cli.Command{
@@ -27,5 +30,66 @@ var Toot = cli.Command{
 			Value: defaultDuration,
 		},
 	},
-	Action: listCalendars,
+	Action: toot,
+}
+
+func errPrint(s string, p ...interface{}) {
+	fmt.Fprintf(os.Stderr, s, p...)
+}
+
+func toot(c *cli.Context) error {
+	var gClient *madon.Client
+	var err error
+
+	// Overwrite variables using Viper
+	AppName := ""
+	AppWebsite := ""
+	scopes := make([]string, 0)
+	instanceURL := ""
+	appID := ""
+	appSecret := ""
+	verbose := true
+
+	if instanceURL == "" {
+		return fmt.Errorf("no instance provided")
+	}
+
+	if verbose {
+		errPrint("Instance: '%s'", instanceURL)
+	}
+
+	if appID != "" && appSecret != "" {
+		// We already have an app key/secret pair
+		gClient, err = madon.RestoreApp(AppName, instanceURL, appID, appSecret, nil)
+		if err != nil {
+			return err
+		}
+		// Check instance
+		if _, err := gClient.GetCurrentInstance(); err != nil {
+			return fmt.Errorf("could not connect to server with provided app ID/secret: %w", err)
+		}
+		if verbose {
+			errPrint("Using provided app ID/secret")
+		}
+		return nil
+	}
+
+	if appID != "" || appSecret != "" {
+		errPrint("Warning: provided app id/secrets incomplete -- registering again")
+	}
+
+	gClient, err = madon.NewApp(AppName, AppWebsite, scopes, madon.NoRedirect, instanceURL)
+	if err != nil {
+		return fmt.Errorf("app registration failed: %w", err)
+	}
+
+	errPrint("Registered new application.")
+
+	var st *madon.Status
+	st, err = gClient.PostStatus("tootText", 0, nil, false, "", "public")
+	if st.URL != "" {
+
+	}
+
+	return err
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module github.com/mariusor/esports-calendar
 go 1.13
 
 require (
+	github.com/McKael/madon v2.3.0+incompatible
 	github.com/PuerkitoBio/goquery v1.5.0
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/soh335/ical v0.0.0-20160115065015-8bf3eeeb3583
 	github.com/urfave/cli v1.22.1
 	go.etcd.io/bbolt v1.3.3
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
 )
